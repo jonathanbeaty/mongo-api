45702ba74c596f7f9897aa5289107b07d29665c4
Jonathan Beaty <jonathanbeaty15@gmail.com>
2018-10-30 19:32:17 -0500

    updating

--- server.js
+++ server.js
@@ -3,13 +3,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-// Mongoose internally uses a promise-like object,
-// but it's better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
 const {
     DATABASE_URL,
     PORT
@@ -21,8 +16,9 @@ const {
 
 const app = express();
 
-app.use(morgan('common')); //middleware logging 
-app.use(express.json()); //middleware body parser
+app.use(morgan('common'));
+app.use(express.json());
+
 
 app.get('/authors', (req, res) => {
     Author
@@ -43,6 +39,7 @@ app.get('/authors', (req, res) => {
             });
         });
 });
+
 
 app.post('/authors', (req, res) => {
     const requiredFields = ['firstName', 'lastName', 'userName'];
@@ -164,24 +161,41 @@ app.delete('/authors/:id', (req, res) => {
         });
 });
 
+
 app.get('/posts', (req, res) => {
     BlogPost
         .find()
         .then(posts => {
-            res.json(posts.map(post => post.serialize()));
+            res.json(posts.map(post => {
+                return {
+                    id: post._id,
+                    author: post.authorName,
+                    content: post.content,
+                    title: post.title
+                };
+            }));
         })
         .catch(err => {
-            console.log(err);
+            console.error(err);
             res.status(500).json({
                 error: 'something went terribly wrong'
             });
         });
 });
 
+
 app.get('/posts/:id', (req, res) => {
     BlogPost
         .findById(req.params.id)
-        .then(post => res.json(post.serialize()))
+        .then(post => {
+            res.json({
+                id: post._id,
+                author: post.authorName,
+                content: post.content,
+                title: post.title,
+                comments: post.comments
+            });
+        })
         .catch(err => {
             console.error(err);
             res.status(500).json({
@@ -190,46 +204,50 @@ app.get('/posts/:id', (req, res) => {
         });
 });
 
+
 app.post('/posts', (req, res) => {
-    const requiredFields = ['title', 'content', 'author'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
+    const requiredFields = ['title', 'content', 'author_id'];
+    requiredFields.forEach(field => {
         if (!(field in req.body)) {
             const message = `Missing \`${field}\` in request body`;
             console.error(message);
             return res.status(400).send(message);
         }
-    }
+    });
 
-    BlogPost
-        .create({
-            title: req.body.title,
-            content: req.body.content,
-            author: req.body.author
+    Author
+        .findById(req.body.author_id)
+        .then(author => {
+            if (author) {
+                BlogPost
+                    .create({
+                        title: req.body.title,
+                        content: req.body.content,
+                        author: req.body.id
+                    })
+                    .then(blogPost => res.status(201).json({
+                        id: blogPost.id,
+                        author: `${author.firstName} ${author.lastName}`,
+                        content: blogPost.content,
+                        title: blogPost.title,
+                        comments: blogPost.comments
+                    }))
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({
+                            error: 'Something went wrong'
+                        });
+                    });
+            } else {
+                const message = `Author not found`;
+                console.error(message);
+                return res.status(400).send(message);
+            }
         })
-        .then(blogPost => res.status(201).json(blogPost.serialize()))
         .catch(err => {
             console.error(err);
             res.status(500).json({
-                error: 'Something went wrong'
-            });
-        });
-
-});
-
-
-app.delete('/posts/:id', (req, res) => {
-    BlogPost
-        .findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).json({
-                message: 'success'
-            });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({
-                error: 'something went terribly wrong'
+                error: 'something went horribly awry'
             });
         });
 });
@@ -243,7 +261,7 @@ app.put('/posts/:id', (req, res) => {
     }
 
     const updated = {};
-    const updateableFields = ['title', 'content', 'author'];
+    const updateableFields = ['title', 'content'];
     updateableFields.forEach(field => {
         if (field in req.body) {
             updated[field] = req.body[field];
@@ -256,19 +274,22 @@ app.put('/posts/:id', (req, res) => {
         }, {
             new: true
         })
-        .then(updatedPost => res.status(204).end())
+        .then(updatedPost => res.status(200).json({
+            id: updatedPost.id,
+            title: updatedPost.title,
+            content: updatedPost.content
+        }))
         .catch(err => res.status(500).json({
-            message: 'Something went wrong'
+            message: err
         }));
 });
 
 
-app.delete('/:id', (req, res) => {
+app.delete('/posts/:id', (req, res) => {
     BlogPost
         .findByIdAndRemove(req.params.id)
         .then(() => {
             console.log(`Deleted blog post with id \`${req.params.id}\``);
-            // .catch
             res.status(204).end();
         });
 });
@@ -288,15 +309,13 @@ let server;
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
     return new Promise((resolve, reject) => {
-        mongoose.connect(databaseUrl, {
-            useNewUrlParser: true
-        }, err => {
+        mongoose.connect(databaseUrl, err => {
             if (err) {
                 return reject(err);
             }
             server = app.listen(port, () => {
                     console.log(`Your app is listening on port ${port}`);
-                    resolve(); //
+                    resolve();
                 })
                 .on('error', err => {
                     mongoose.disconnect();
